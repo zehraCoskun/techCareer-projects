f1a6af1ed49ea3995c9b30118b2ef54f8ffe33ce
Zehra Coskun <zehraxcoskun@gmail.com>
2023-09-04 17:19:22 +0300

    Initial commit

---
+++ Homework1.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+let sehir = "Manisa"
+let ulke = "Türkiye"
+let telefon = "0555 555 55 55"
+let postaKodu = "10010"
+let email = "zehra@mail.com"
+let meslek = "iOS Developer"
+let stokMiktarı : Int = 28
+let musteriAdı = "Akın"
+let bakiye : Double = 14.99
+let dogumGunu = "05.09.1996"
+let maas : Int = 35000
+let medeniDurum = "Evli"
+let urunYorum = "harika bir ürün"
+
+var odemeTarihi = "01.01.2014"
+var odeme = false
+var siparisAdet = 3
+var arabaModel = "Fiat"
+var kitapAdi = "Yanılgının İcadı"
+var yayinlamaTarihi = "23.05.2020"
+var indirimMiktari : Double = 12.35
+var odaSayisi = 4
+var enlem: Double = 26.09090
+var boylam: Double = 45.3030
+var yemekFiyati: Double = 99.99
+
+let marka = "Apple"
+let muzikAdi = "Heç Vaxt"
+let videoSuresi: Double = 12.45
+let urunPuanı: Int = 4
+let resimAdi = "Kelebekler ve Figür"
+let dosyaFormati = ".txt"
+let renk = "Açık gökyüzü mavisi"
+let renkKodu = "87CEFA"
+let telefonModeli = "iPhone XR"
+let ekranBoyutu: Double = 6.06
+let agirlik: Int = 194
+
+ 
+print("Ben\(marka) 'ın \(telefonModeli) model telefonunu kullanıyorum.")
